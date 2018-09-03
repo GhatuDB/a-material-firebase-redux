40b527acdee5e9b472da77983c8170b0bfe0eec5
Shailender Singh <ghatudb@gmail.com>
2018-09-03 12:10:27 +0300

    ngrx store added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from "@angular/forms";
+// Ngrx added
+import { StoreModule } from "@ngrx/store";
+import { postReducer } from "./reducers/post.reducer";
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FormsModule,
+    StoreModule.forRoot({post: postReducer})
   ],
   declarations: [DashboardComponent]
 })
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 
+import { Post } from "../models/post.model";
+import * as PostActions from "../actions/post.actions";
+
+interface AppState {
+  post: Post;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  post$: Observable<Post>
+  text: string;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { 
+    this.post$ = this.store.select('post')
+  }
 
   ngOnInit() {
+  }
+
+  editText(){
+    this.store.dispatch(new PostActions.EditText(this.text))
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset())
+  }
+
+  upVote() {
+    this.store.dispatch(new PostActions.Upvote())
+  }
+
+  downVote() {
+    this.store.dispatch(new PostActions.Downvote())
   }
 
 }
